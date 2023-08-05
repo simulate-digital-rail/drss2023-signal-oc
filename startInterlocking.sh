2a9261acf5c7dc105e7191c20e54eed9494196cb
Merlin Stollenwerk <merlin-stollenwerk@past-development.de>
2023-08-05 15:41:21 +0200

    Added startup scripts

---
+++ startInterlocking.sh
@@ -0,0 +1,3 @@
+./rasta-protocol/build/rasta_grpc_bridge_udp config/rasta_interlocking.cfg \
+0.0.0.0:4242 127.0.0.1 9998 127.0.0.1 9999 97 96 127.0.0.1:50001 
+#cargo run --bin sender 0.0.0.0 50001 config/pin_config.toml
