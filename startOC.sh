8aa2b8719a19b2b9bfcda0ce87036834f36c2d59
Merlin Stollenwerk <merlin-stollenwerk@past-development.de>
2023-08-30 11:46:39 +0200

    Correctly killing background processes when startup scripts are killed with Ctrl+C

--- startOC.sh
+++ startOC.sh
@@ -1,3 +1,5 @@
+trap 'kill $OCID; exit' INT
 ./rasta-protocol/build/rasta_grpc_bridge_udp config/rasta_oc.cfg \
 0.0.0.0:50002 127.0.0.1 8888 127.0.0.1 8889 96 97 &
-cargo run --bin receiver 127.0.0.1 50002 config/pin_config.toml 
+OCID=$!
+cargo run --bin receiver 127.0.0.1 50002 config/pin_config.toml
